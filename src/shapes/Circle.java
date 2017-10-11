79dd02c75048d948111774f9e6ca5406e5910c53
qiye <qiye@csee137aer18.lcsee.wvu.edu>
2017-10-11 19:13:42 -0400

    Qing

--- src/shapes/Circle.java
+++ src/shapes/Circle.java
@@ -2,19 +2,30 @@ package shapes;
 
 public class Circle extends Shape {
 	private double radius;
-	
+
 	/*
 	 * Complete the implementation of this class by finishing the constructor
-	 * below.  Circles must have a positive radius.
+	 * below. Circles must have a positive radius.
 	 */
+
 	public Circle(double radius) throws IllegalArgumentException {
-		// TODO Implement me
+
+		if (radius > 0)
+			this.radius = radius;
+		else
+			throw new IllegalArgumentException("Radius must be positive!");
+
 	}
-	
+
 	/*
-	 *  Implement any remaining methods demanded by the superclass.
+	 * Implement any remaining methods demanded by the superclass.
 	 */
-	
+
+	@Override
+	public double calculateArea() {
+		return Math.PI * Math.pow(radius, 2);
+	}
+
 	public double getRadius() {
 		return radius;
 	}
---
+++ src/tests/CircleTest.java
@@ -0,0 +1,35 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import shapes.Circle;
+
+public class CircleTest {
+	private final double DELTA = 0.00000000000001;
+	
+	@Test
+	public void testCalculateArea() {
+		Circle a = new Circle(1);
+		assertEquals(Math.PI * Math.pow(1, 2), a.calculateArea(), DELTA);
+	}
+
+	@Test
+	public void testCircle() {
+		Circle b = new Circle(1);
+		assertEquals(1, b.getRadius(), DELTA);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void textNegativeCircle(){
+		new Circle(-1);
+	}
+
+	@Test
+	public void testGetRadius() {
+		Circle c = new Circle(1);
+		assertEquals(1, c.getRadius(), DELTA);
+	}
+
+}
